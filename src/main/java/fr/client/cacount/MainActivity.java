48ab06a84d0fbc8499c49faa32ae4ce1d399d5af
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2017-02-11 17:20:42 +0100

    Display Notification

--- src/main/java/fr/client/cacount/MainActivity.java
+++ src/main/java/fr/client/cacount/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.client.cacount;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +15,14 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	    this.setFinishOnTouchOutside(true);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("Notification");
+        builder.setContentText("Tu es à la deche!");
+        builder.setSmallIcon(R.drawable.ic_launcher);
+        Notification notification = builder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
