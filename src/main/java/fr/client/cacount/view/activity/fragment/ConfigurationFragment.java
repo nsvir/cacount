11b99a4cc54fff808c56957ed1608dd786dad28c
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2017-03-05 16:42:17 +0100

    Settings display value

--- src/main/java/fr/client/cacount/view/activity/fragment/ConfigurationFragment.java
+++ src/main/java/fr/client/cacount/view/activity/fragment/ConfigurationFragment.java
@@ -3,7 +3,10 @@ package fr.client.cacount.view.activity.fragment;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
 import fr.client.cacount.Cacount;
 import fr.client.cacount.R;
 import fr.client.cacount.view.activity.UpdateActivity;
@@ -19,11 +22,32 @@ public class ConfigurationFragment extends PreferenceFragment implements SharedP
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        initSummary(getPreferenceScreen());
+    }
+
+    private void initSummary(Preference p) {
+        if (p instanceof PreferenceGroup) {
+            PreferenceGroup pGrp = (PreferenceGroup) p;
+            for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+                initSummary(pGrp.getPreference(i));
+            }
+        } else {
+            updatePreference(p);
+        }
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updatePreference(findPreference(key));
         UpdateActivity.updateNotification(getActivity());
+    }
+
+    private void updatePreference(Preference preference) {
+        if (preference instanceof EditTextPreference) {
+            EditTextPreference pref = (EditTextPreference) preference;
+            pref.setSummary(pref.getText());
+        }
     }
 }
